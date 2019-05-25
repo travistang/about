46044bc6e08ca57b73de12db78e29e74f5c55948
Travis <ga86yam@mytum.de>
2019-05-25 23:47:29 +0200

    Updates

---
+++ precache-manifest.ed608b78de6e299c00b6b4b61e799329.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c15157a99b2899c727a4294a338369c",
+    "url": "/about/static/media/formal-avatar.5c15157a.jpg"
+  },
+  {
+    "revision": "9bc58bf4218f948abd1885a86393793d",
+    "url": "/about/static/media/avatar.9bc58bf4.JPG"
+  },
+  {
+    "revision": "2eb993563b473bafd9dd",
+    "url": "/about/static/js/runtime~main.2eb99356.js"
+  },
+  {
+    "revision": "bb3c422cb0d5dd866a86",
+    "url": "/about/static/js/main.bb3c422c.chunk.js"
+  },
+  {
+    "revision": "79f21f1150683724f30c",
+    "url": "/about/static/js/1.79f21f11.chunk.js"
+  },
+  {
+    "revision": "bb3c422cb0d5dd866a86",
+    "url": "/about/static/css/main.617c4ebb.chunk.css"
+  },
+  {
+    "revision": "1018bccf9c8e244722929fd7c2a13c27",
+    "url": "/about/index.html"
+  }
+];
